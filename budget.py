f47110fe649e1023dc337b4c8e88ccde76acb8ae
Nick Le Large <nick@le-large.com>
2023-08-13 15:18:15 +0200

    deposit & withdraw

--- budget.py
+++ budget.py
@@ -3,6 +3,21 @@ class Category:
         self.name = name
         self.ledger = []
 
+    def deposit(self, amount, description = ""):
+        self.ledger.append({
+            "amount": amount,
+            "description": description
+        })
+
+    def withdraw(self, amount, description = ""):
+        if not self.check_funds(amount):
+            return False
+
+        self.ledger.append({
+            "amount": -amount,
+            "description": description
+        })
+        return True
 
     def get_balance(self):
         return sum(item["amount"] for item in self.ledger)
