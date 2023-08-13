f0bd99a76a915be4916defdc044d065fb9875f35
Nick Le Large <nick@le-large.com>
2023-08-13 16:07:16 +0200

    implement __str__

--- budget.py
+++ budget.py
@@ -1,3 +1,5 @@
+import math
+
 class Category:
     def __init__(self, name):
         self.name = name
@@ -34,6 +36,29 @@ class Category:
         self.withdraw(amount, "Transfer to " + category.name)
         category.deposit(amount, "Transfer from " + self.name)
         return True
+
+    def __str__(self):
+        res = self.title_str()
+        item_strings = (self.item_str(item) for item in self.ledger)
+        res += "".join(item_strings)
+        res += self.total_str()
+        return res
+    
+    def title_str(self, title_length = 30):
+        name_length = len(self.name)
+        prefix = math.ceil((title_length - name_length) / 2) * "*"
+        suffix = math.ceil((title_length - name_length) / 2) * "*"
+        return prefix + self.name + suffix + "\n"
+    
+    def item_str(self, item, string_length = 30):
+        name = item["description"][:23]
+        amount = "{:.2f}".format(item["amount"])[:7]
+        fill = (string_length - len(name) - len(amount)) * " "
+        return name + fill + amount + "\n"
+    
+    def total_str(self):
+        return "Total: " +  "{:.2f}".format((self.get_balance()))
     
 
 def create_spend_chart(categories):
+    pass
