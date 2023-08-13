9cee262c7c5c357411af84f1555e77505465ce1b
Nick Le Large <nick@le-large.com>
2023-08-13 15:17:50 +0200

    check_funds

--- budget.py
+++ budget.py
@@ -4,5 +4,13 @@ class Category:
         self.ledger = []
 
 
+    def get_balance(self):
+        return sum(item["amount"] for item in self.ledger)
+    
+    def check_funds(self, amount):
+        if amount <= self.get_balance():
+            return True
+        return False
+    
 
 def create_spend_chart(categories):
